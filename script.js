8885f5a18f0b9f08021ec08a7187207f5064e9d7
thealmightymoonlord <wangeddie9@gmail.com>
2026-05-26 12:50:13 -0400

    app sends verification code

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ document.getElementById('verification-form').addEventListener('submit', function
   event.preventDefault();
 
   const username = document.getElementById('username').value;
-  const email = document.getElementById('user-email').value;
+  const email = document.getElementById('email').value;
   
   // Generate a random 6-digit verification code
   const verificationCode = Math.floor(100000 + Math.random() * 900000);
-
+  const formattedTime = new Date(Date.now() + 3 * 60 * 1000).toLocaleTimeString([], { 
+    hour: '2-digit', 
+    minute: '2-digit' 
+  });
   // Store this code in localStorage or a global variable to verify it later
   sessionStorage.setItem('sentCode', verificationCode);
 
   // Template parameters for EmailJS
   const templateParams = {
-    to_name: username,
+    name: username,
     to_email: email,
-    verification_code: verificationCode
+    passcode: verificationCode,
+    time: formattedTime
   };
+
+  console.log("What JS is sending to EmailJS:", templateParams);
 
   // Send the email
   emailjs.send('service_vsor7t5', 'template_we9ahji', templateParams)
